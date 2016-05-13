5a00a3a3f254c7a157bbcb3b94b377ce80b4987b
JollyRoberts <jolly.development@gmail.com>
2016-05-13 11:56:55 -0400

    updates to compress-files.nemo_action to allow it to show with mulitple files selected

--- compress-files.sh
+++ compress-files.sh
@@ -3,9 +3,12 @@
 #variables
 #grab the first paramater sent by the .nemo_action (%P - insert path of parent (current) directory)
 DIR=$1
+#grab the second paramater sent by the .nemo_action (%f the display name of the first file)
+NAME=new
+#NAME=`date +%Y%m%d-%H%M`
 #grab the rest of the paramaters, starting at $2, since $1 is used above. This is the list of files)
 FILES=${@:2}
 
 
 #zip the files to a new.zip in the correct directory, junking the /path/ of the files
-zip -j $1/new.zip $FILES
+zip -j $DIR/$NAME.zip $FILES
