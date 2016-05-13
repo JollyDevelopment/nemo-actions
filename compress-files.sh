b42991368f4c6eca3c0d81e8a1d3bd0c0265f345
JollyRoberts <jolly.development@gmail.com>
2016-05-13 12:31:33 -0400

    updates to compress-files.sh to use the date command to generate the new .zip name

--- compress-files.sh
+++ compress-files.sh
@@ -4,8 +4,8 @@
 #grab the first paramater sent by the .nemo_action (%P - insert path of parent (current) directory)
 DIR=$1
 #grab the second paramater sent by the .nemo_action (%f the display name of the first file)
-NAME=new
-#NAME=`date +%Y%m%d-%H%M`
+#NAME=new
+NAME=`date +%Y%m%d-%H%M`
 #grab the rest of the paramaters, starting at $2, since $1 is used above. This is the list of files)
 FILES=${@:2}
 
