f9a552c8062a39e7c24ba0dd74f138e0d0c11409
JollyRoberts <jolly.development@gmail.com>
2016-05-13 16:47:11 -0400

    updates to .sh file variable names and comments

--- compress-files.sh
+++ compress-files.sh
@@ -2,13 +2,14 @@
 
 #variables
 #grab the first paramater sent by the .nemo_action (%P - insert path of parent (current) directory)
-DIR=$1
-#grab the second paramater sent by the .nemo_action (%f the display name of the first file)
-#NAME=new
-NAME=`date +%Y%m%d-%H%M`
+dir=$1
+
+#use the date command to generate the name of the new file. YearMonthDay-HourMinute.zip
+name=`date +%Y%m%d-%H%M`
+
 #grab the rest of the paramaters, starting at $2, since $1 is used above. This is the list of files)
-FILES=${@:2}
+files=${@:2}
 
 
 #zip the files to a new.zip in the correct directory, junking the /path/ of the files
-zip -j $DIR/$NAME.zip $FILES
+zip -j $dir/$name.zip $files
